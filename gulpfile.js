03853773cb656ec4e2ff26ec427b3b658b68b41a
SNX <52723510+SNXdevelopment@users.noreply.github.com>
2023-02-11 05:19:59 +0100

    beta-0.1

--- gulpfile.js
+++ gulpfile.js
@@ -1,177 +1,145 @@
 "use strict";
 
-const sass = require("gulp-sass")(require("sass"));
-const gulp = require("gulp");
-const sourcemaps = require("gulp-sourcemaps");
-const fileinclude = require("gulp-file-include");
+// Load plugins
 const autoprefixer = require("gulp-autoprefixer");
-const bs = require("browser-sync").create();
-const rimraf = require("rimraf");
-const comments = require("gulp-header-comment");
+const browsersync = require("browser-sync").create();
+const cleanCSS = require("gulp-clean-css");
+const del = require("del");
+const gulp = require("gulp");
+const header = require("gulp-header");
+const merge = require("merge-stream");
+const plumber = require("gulp-plumber");
+const rename = require("gulp-rename");
+const sass = require("gulp-sass");
+const uglify = require("gulp-uglify");
 
-var path = {
-  src: {
-    html: "source/*.html",
-    others: "source/*.+(php|ico|png)",
-    htminc: "source/partials/**/*.htm",
-    incdir: "source/partials/",
-    plugins: "source/plugins/**/*.*",
-    js: "source/js/*.js",
-    scss: "source/scss/**/*.scss",
-    images: "source/images/**/*.+(png|jpg|gif|svg)",
-  },
-  build: {
-    dirBuild: "theme/",
-    dirDev: "theme/",
-  },
-};
+// Load package.json for banner
+const pkg = require('./package.json');
 
-// HTML
-gulp.task("html:build", function () {
+// Set the banner content
+const banner = ['/*!\n',
+  ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
+  ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
+  ' * Licensed under <%= pkg.license %> (https://github.com/StartBootstrap/<%= pkg.name %>/blob/master/LICENSE)\n',
+  ' */\n',
+  '\n'
+].join('');
+
+// BrowserSync
+function browserSync(done) {
+  browsersync.init({
+    server: {
+      baseDir: "./"
+    },
+    port: 3000
+  });
+  done();
+}
+
+// BrowserSync reload
+function browserSyncReload(done) {
+  browsersync.reload();
+  done();
+}
+
+// Clean vendor
+function clean() {
+  return del(["./vendor/"]);
+}
+
+// Bring third party dependencies from node_modules into vendor directory
+function modules() {
+  // Bootstrap JS
+  var bootstrapJS = gulp.src('./node_modules/bootstrap/dist/js/*')
+    .pipe(gulp.dest('./vendor/bootstrap/js'));
+  // Bootstrap SCSS
+  var bootstrapSCSS = gulp.src('./node_modules/bootstrap/scss/**/*')
+    .pipe(gulp.dest('./vendor/bootstrap/scss'));
+  // ChartJS
+  var chartJS = gulp.src('./node_modules/chart.js/dist/*.js')
+    .pipe(gulp.dest('./vendor/chart.js'));
+  // dataTables
+  var dataTables = gulp.src([
+      './node_modules/datatables.net/js/*.js',
+      './node_modules/datatables.net-bs4/js/*.js',
+      './node_modules/datatables.net-bs4/css/*.css'
+    ])
+    .pipe(gulp.dest('./vendor/datatables'));
+  // Font Awesome
+  var fontAwesome = gulp.src('./node_modules/@fortawesome/**/*')
+    .pipe(gulp.dest('./vendor'));
+  // jQuery Easing
+  var jqueryEasing = gulp.src('./node_modules/jquery.easing/*.js')
+    .pipe(gulp.dest('./vendor/jquery-easing'));
+  // jQuery
+  var jquery = gulp.src([
+      './node_modules/jquery/dist/*',
+      '!./node_modules/jquery/dist/core.js'
+    ])
+    .pipe(gulp.dest('./vendor/jquery'));
+  return merge(bootstrapJS, bootstrapSCSS, chartJS, dataTables, fontAwesome, jquery, jqueryEasing);
+}
+
+// CSS task
+function css() {
   return gulp
-    .src(path.src.html)
-    .pipe(
-      fileinclude({
-        basepath: path.src.incdir,
-      })
-    )
-    .pipe(
-      comments(`
-    WEBSITE: https://themefisher.com
-    TWITTER: https://twitter.com/themefisher
-    FACEBOOK: https://www.facebook.com/themefisher
-    GITHUB: https://github.com/themefisher/
-    `)
-    )
-    .pipe(gulp.dest(path.build.dirDev))
-    .pipe(
-      bs.reload({
-        stream: true,
-      })
-    );
-});
+    .src("./scss/**/*.scss")
+    .pipe(plumber())
+    .pipe(sass({
+      outputStyle: "expanded",
+      includePaths: "./node_modules",
+    }))
+    .on("error", sass.logError)
+    .pipe(autoprefixer({
+      cascade: false
+    }))
+    .pipe(header(banner, {
+      pkg: pkg
+    }))
+    .pipe(gulp.dest("./css"))
+    .pipe(rename({
+      suffix: ".min"
+    }))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest("./css"))
+    .pipe(browsersync.stream());
+}
 
-// SCSS
-gulp.task("scss:build", function () {
+// JS task
+function js() {
   return gulp
-    .src(path.src.scss)
-    .pipe(sourcemaps.init())
-    .pipe(
-      sass({
-        outputStyle: "expanded",
-      }).on("error", sass.logError)
-    )
-    .pipe(autoprefixer())
-    .pipe(sourcemaps.write("/"))
-    .pipe(
-      comments(`
-    WEBSITE: https://themefisher.com
-    TWITTER: https://twitter.com/themefisher
-    FACEBOOK: https://www.facebook.com/themefisher
-    GITHUB: https://github.com/themefisher/
-    `)
-    )
-    .pipe(gulp.dest(path.build.dirDev + "css/"))
-    .pipe(
-      bs.reload({
-        stream: true,
-      })
-    );
-});
+    .src([
+      './js/*.js',
+      '!./js/*.min.js',
+    ])
+    .pipe(uglify())
+    .pipe(header(banner, {
+      pkg: pkg
+    }))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('./js'))
+    .pipe(browsersync.stream());
+}
 
-// Javascript
-gulp.task("js:build", function () {
-  return gulp
-    .src(path.src.js)
-    .pipe(
-      comments(`
-  WEBSITE: https://themefisher.com
-  TWITTER: https://twitter.com/themefisher
-  FACEBOOK: https://www.facebook.com/themefisher
-  GITHUB: https://github.com/themefisher/
-  `)
-    )
-    .pipe(gulp.dest(path.build.dirDev + "js/"))
-    .pipe(
-      bs.reload({
-        stream: true,
-      })
-    );
-});
+// Watch files
+function watchFiles() {
+  gulp.watch("./scss/**/*", css);
+  gulp.watch(["./js/**/*", "!./js/**/*.min.js"], js);
+  gulp.watch("./**/*.html", browserSyncReload);
+}
 
-// Images
-gulp.task("images:build", function () {
-  return gulp
-    .src(path.src.images)
-    .pipe(gulp.dest(path.build.dirDev + "images/"))
-    .pipe(
-      bs.reload({
-        stream: true,
-      })
-    );
-});
+// Define complex tasks
+const vendor = gulp.series(clean, modules);
+const build = gulp.series(vendor, gulp.parallel(css, js));
+const watch = gulp.series(build, gulp.parallel(watchFiles, browserSync));
 
-// Plugins
-gulp.task("plugins:build", function () {
-  return gulp
-    .src(path.src.plugins)
-    .pipe(gulp.dest(path.build.dirDev + "plugins/"))
-    .pipe(
-      bs.reload({
-        stream: true,
-      })
-    );
-});
-
-// Other files like favicon, php, sourcele-icon on root directory
-gulp.task("others:build", function () {
-  return gulp.src(path.src.others).pipe(gulp.dest(path.build.dirDev));
-});
-
-// Clean Build Folder
-gulp.task("clean", function (cb) {
-  rimraf("./theme", cb);
-});
-
-// Watch Task
-gulp.task("watch:build", function () {
-  gulp.watch(path.src.html, gulp.series("html:build"));
-  gulp.watch(path.src.htminc, gulp.series("html:build"));
-  gulp.watch(path.src.scss, gulp.series("scss:build"));
-  gulp.watch(path.src.js, gulp.series("js:build"));
-  gulp.watch(path.src.images, gulp.series("images:build"));
-  gulp.watch(path.src.plugins, gulp.series("plugins:build"));
-});
-
-// Dev Task
-gulp.task(
-  "default",
-  gulp.series(
-    "clean",
-    "html:build",
-    "js:build",
-    "scss:build",
-    "images:build",
-    "plugins:build",
-    "others:build",
-    gulp.parallel("watch:build", function () {
-      bs.init({
-        server: {
-          baseDir: path.build.dirDev,
-        },
-      });
-    })
-  )
-);
-
-// Build Task
-gulp.task(
-  "build",
-  gulp.series(
-    "html:build",
-    "js:build",
-    "scss:build",
-    "images:build",
-    "plugins:build"
-  )
-);
+// Export tasks
+exports.css = css;
+exports.js = js;
+exports.clean = clean;
+exports.vendor = vendor;
+exports.build = build;
+exports.watch = watch;
+exports.default = build;
